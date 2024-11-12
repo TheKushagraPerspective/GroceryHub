ab3241c55e000ebf1e9019ee9da35c18265fc19e
Kushagra Varshney <118505260+TheKushagraPerspective@users.noreply.github.com>
2024-11-12 12:24:03 +0530

    Update main_kirana.jsx

--- frontend/src/components/products/main_kirana.jsx
+++ frontend/src/components/products/main_kirana.jsx
@@ -18,7 +18,7 @@ const MainKiranaItems = () => {
     const handleProduct = async (e) => {
         // e.preventDefault();
         try {
-            const response = await fetch(`http://localhost:2000/api/product/${category}`);
+            const response = await fetch(`https://groceryshop24-backend.onrender.com/api/product/${category}`);
             const data = await response.json();
             if (response.status === 200) {
                 alert('Product fetched successfully');
@@ -52,7 +52,7 @@ const MainKiranaItems = () => {
         }
 
         try{
-            const response = await fetch("http://localhost:2000/api/add/cart" , {
+            const response = await fetch("https://groceryshop24-backend.onrender.com/api/add/cart" , {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
